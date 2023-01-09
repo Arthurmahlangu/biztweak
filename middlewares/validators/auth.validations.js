a560b81733a19bf94cc44d13058a4fb1c4b19b5e
lebohang0824 <lebohang0824@gmail.com>
2023-01-09 10:30:51 +0200

    Add login validation

--- middlewares/validators/auth.validations.js
+++ middlewares/validators/auth.validations.js
@@ -5,7 +5,24 @@ const { body } = require("express-validator")
  * @type {ValidationChain[]}
  */
 exports.authValidator = [
-    body("fullname").exists().withMessage("Fullname is required"),
-    body("email").exists().isEmail().withMessage("Invalid Email"),
-    body("password").exists().isStrongPassword().withMessage("Password not meeting minimum requirements")
+    body("fullname")
+        .exists().withMessage("Fullname is required"),
+    body("email")
+        .exists().withMessage("Invalid Email")
+        .isEmail().withMessage("Invalid Email"),
+    body("password")
+        .exists().withMessage("Password required")
+        .isStrongPassword().withMessage("Password not meeting minimum requirements")
+]
+
+/**
+ *
+ * @type {ValidationChain[]}
+ */
+exports.loginValidator = [
+    body("email")
+        .exists().withMessage("Invalid Email")
+        .isEmail().withMessage("Invalid Email"),
+    body("password")
+        .exists().withMessage("Password required")
 ]
--- routes/auth.api.js
+++ routes/auth.api.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const AuthController = require("../controllers/auth.controller")
 
 const { validate } = require("../middlewares/validators/wrapper.validator")
-const { authValidator } = require("../middlewares/validators/auth.validations")
+const { authValidator, loginValidator } = require("../middlewares/validators/auth.validations")
 
-router.post("/email-auth", AuthController.emailAuth)
+router.post("/email-auth", validate(loginValidator), AuthController.emailAuth)
 router.post("/email-register", validate(authValidator), AuthController.emailRegister)
 
 module.exports = router
